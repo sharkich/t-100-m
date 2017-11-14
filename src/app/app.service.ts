eac0473ae7f00e192eb0c7c5cec2764a117981b4
Temich Koziar <a@temich.in.ua>
2017-11-14 23:35:50 +0200

    :airplane: Match Numbers 0-10

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -9,16 +9,52 @@ const CELLULARS = {
     'PEOPLEnet': [92],
     'Intertelecom': [94]
 };
+
+const NUMBERS_100 = [ // http://mnemonikon.ru/members/spr_f.htm
+    'номерок',
+    'еЖ',
+    'яД',
+    'уХо',
+    'Чай',
+    'оБои',
+    'юЛа',
+    'оСа',
+    'иВа',
+    'яйЦо',
+    'оГоНь' // 10
+];
+
 export class AppService {
-    
+
     static match(text: string) {
-        const searchNumber = parseInt(text);
-        const cellular = Object.keys(CELLULARS).find((key) => {
-            return CELLULARS[key].indexOf(searchNumber) !== -1;
-        });
+        const cellular = this.getCellular(text);
         if (cellular) {
             return cellular;
         }
+        const number = this.getNumbers(text);
+        if (number) {
+            return number;
+        }
     }
-    
+
+    static getCellular(text: string) {
+        const number = parseInt(text);
+        return Object.keys(CELLULARS).find((key) => CELLULARS[key].indexOf(number) !== -1);
+    }
+
+    static getNumbers(text: string) {
+        if (text === '0') {
+            return 'номерок';
+        }
+        if (text === '00') {
+            return 'бочки';
+        }
+        if (text === '000') {
+            return 'МаНоМетр';
+        }
+        const number = parseInt(text);
+        if (number >= 0 && number <= 100) {
+            return NUMBERS_100[number];
+        }
+    }
 }
